c4ba1fd41d34d54cd0392e13c59222d43fe33e10
1Gregory <6squaress@gmail.com>
2020-01-14 20:44:27 +0300

    Added url of release

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from fool_house import __version__
 with open("README.md") as f:
     long_description = f.read()
 
-with open("requirements.txt") as f:
-    install_requirements = filter(lambda string: string != "", f.read().split("\n"))
-
 setup(
     name="Fool-house",
     description="Yellow house",
@@ -15,9 +12,9 @@ setup(
     author="Andreew Gregory",
     author_email='6squaress@gmail.com',
     url="https://github.com/1Gregory/fool-house",
-    download_url="",
+    download_url="https://github.com/1Gregory/fool-house/archive/1.0.tar.gz",
     version=__version__,
-    install_requires=install_requirements,
+    install_requires=[],
     packages=["fool_house"],
     license="MIT",
 )
