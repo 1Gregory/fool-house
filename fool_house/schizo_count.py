1999efc99e84bfb3a4517c59888d39e86f282e7a
1Gregory <6squaress@gmail.com>
2020-01-14 18:43:37 +0300

    Прокачан счёт древних шизов

--- fool_house/schizo_count.py
+++ fool_house/schizo_count.py
@@ -15,10 +15,6 @@ schizo_count = [
 def from_int_to_schizo_str(num):
     base = len(schizo_count)  # 10 digits
     res = []
-    while True:
-        r = num % base
-        res.append(schizo_count[r])
-        num //= base
-        if num == 0:
-            break
-    return ".".join(res[::-1])
+    for ch in str(num):
+        res.append(schizo_count[int(ch, base)])
+    return "-".join(res)
